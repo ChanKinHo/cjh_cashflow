7ba39dce8e0ba4141e25ae0c499a3ef75ea619af
chankinho <chankinho@qq.com>
2021-06-03 21:48:20 +0800

    第一次提交，刚建项目

---
+++ src/main/java/com/house/cjh_cashflow/CjhCashflowApplication.java
@@ -0,0 +1,13 @@
+package com.house.cjh_cashflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjhCashflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjhCashflowApplication.class, args);
+    }
+
+}
